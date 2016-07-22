8b3d17e29aba55c2f2ad8b98b9c6c4540aef863b
Chris Enríquez <christoferen7@gmail.com>
2016-07-22 10:01:01 -0600

    Agregando footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,11 @@
+<footer class="container-fluid">
+	<div class="row">
+		<div class="col-lg-12 text-center">
+			<div class="footer-text">
+				<a href="https://twitter.com/khrizenriquez">
+					<label>@khrizenriquez</label>
+				</a>
+			</div>
+		</div>
+	</div>
+</footer>
